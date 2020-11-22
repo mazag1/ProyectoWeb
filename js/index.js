64ae4f4e825f55becd2f73fc42fb8ca3f32c6a95
Marco Antonio <marco_aycachi@outlook.com>
2020-11-22 12:46:42 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
-var arreglo = new Array(5);
+var banner = new Array(5);
 
 for (let index = 0; index < 5; index++) {
-     arreglo[index] = "img/" + (index + 1) + ".jpg";
+    banner[index] = "/imagenes/banner/" + (index + 1) + ".jpg";
 }
 
 window.onload=function()
 {	
+    m_carrusel()
     var nombre=document.getElementById("Quienes");
     nombre.innerHTML="Quienes Somos";
     
-    var cabecera=document.getElementById("cabecera");
-    cabecera.style.background = "url('/imagenes/banner/encabe.jpg')";
-    //m_carrusel()
-
+    document.body.style.backgroundColor = '#FAC67C';
 }
 
 function m_carrusel(){
-    contador = numerorandom(1, 6);
-    //---
-    var cabecera=document.getElementById("Quienes");
-    cabecera.style.backgroundImage  = 'red';
-    //---
+    // contador = numerorandom(1, 6);
+    // //---
+    // var cabecera=document.getElementById("Quienes");
+    // cabecera.style.backgroundImage  = 'red';
+    // //---
+    var cabecera=document.getElementById("cabecera");
+    cabecera.style.background = "url('/imagenes/banner/encabe.jpg')";
+    console.log(9);
     setTimeout("m_carrusel()",3000);
 }
 
