c6a547f73f9b26f30ea0f45a11f8195049ea8d63
YersonC01 <74845635+YersonC01@users.noreply.github.com>
2020-11-22 23:31:40 -0500

    Cambios

---
+++ js/seccion.js
@@ -0,0 +1,31 @@
+//eventos
+var eventos = new Array(6);
+// noticias
+var noticias = new Array(6);
+// concursos
+var concursos = new Array(6);
+//otros
+for (let novedad = 0; novedad < 6; novedad++) {
+    eventos[novedad] = "imagenes/eventos/"+(novedad+ 1)+".jpg";
+    noticias[novedad] = "imagenes/noticias/" + (novedad+ 1) + ".jpg";
+    concursos[novedad] = "imagenes/concursos/" + (novedad+ 1) + ".jpg";
+}
+
+window.onload=function()
+{	
+    m_carrusel()
+    var nombre=document.getElementById("Novedades");
+    nombre.innerHTML="Novedades";
+    document.body.style.backgroundColor = '#FAC67C';
+}
+
+function m_carrusel(){
+    window.document["img_novedades"].src = eventos[numerorandom(1, 6)-1];
+    window.document["img_noticias"].src = noticias[numerorandom(1, 6)-1];
+    window.document["img_cursos"].src = concursos[numerorandom(1, 6)-1];
+    setTimeout("m_carrusel()",3000);
+}
+
+function numerorandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
